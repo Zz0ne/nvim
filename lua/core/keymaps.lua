7939411740a4e4d415afe098d85a7e45ba8af4c3
Nuno <nunotechwork@gmail.com>
2023-07-09 12:31:36 +0100

    fix formatter

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -2,7 +2,7 @@ local opts = { noremap = true, silent = true }
 
 local term_opts = { silent = true }
 
--- Shorten function name
+ -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
 --Remap space as leader key
